d1fa62ab8502a43fe44331a2711ff1f265b985eb
Nicolai Gjellestad <ngjellestad@gmail.com>
2016-08-07 13:22:37 +0200

    Add migration scripts for database upgrade and downgrade

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+"""Local config is not committed to source control, please create a local_config.py with the 
+properties imported from local_config.
+"""
+from local_config import DATABASE_USER, DATABASE_PASSWORD
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Uttorty4545!!@localhost/fylltankene'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@localhost/fylltankene'.format(DATABASE_USER, DATABASE_PASSWORD)
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
